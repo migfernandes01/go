18955275ff82ad2bb74a6d86534bec14b9429652
Miguel Fernandes <miguel.fernandes@outreach.io>
2022-07-02 21:32:40 -0400

    conditionals in go

--- booking-app/main.go
+++ booking-app/main.go
@@ -26,6 +26,7 @@ func main() {
 	var bookings[]string
 
 	// loop through user's input code
+	// infinte loop
 	for {
 		// define vars
 		var firstName string
@@ -49,34 +50,62 @@ func main() {
 		// scan method takes pointer of var
 		fmt.Scan(&ticketAmount)
 
-		remainingTickets = remainingTickets - ticketAmount
+		// bool var for name validity
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		// bool var for email validity
+		isEmailValid := strings.Contains(email, "@")
+		// bool var for tickets amount validity
+		isTicketsAmountValid := ticketAmount > 0 && ticketAmount < remainingTickets
 
-		// assign user's input to the first element of bookings array
-		// bookings[0] = firstName + " " + lastName
+		// check validations	
+		if isValidName && isEmailValid && isTicketsAmountValid{
+			remainingTickets = remainingTickets - ticketAmount
 
-		// append info to next position in slice
-		bookings = append(bookings, firstName + " " + lastName)
+			// assign user's input to the first element of bookings array
+			// bookings[0] = firstName + " " + lastName
 
-		// slice info
-		// fmt.Printf("\nBookings slice: %v\n", bookings)
-		// fmt.Printf("First value: %v\n", bookings[0])
-		// fmt.Printf("Slice type: %T\n", bookings)
-		// fmt.Printf("Slice length: %v\n\n", len(bookings))
+			// append info to next position in slice
+			bookings = append(bookings, firstName + " " + lastName)
 
-		// define string slice
-		firstNames := []string{}
+			// slice info
+			// fmt.Printf("\nBookings slice: %v\n", bookings)
+			// fmt.Printf("First value: %v\n", bookings[0])
+			// fmt.Printf("Slice type: %T\n", bookings)
+			// fmt.Printf("Slice length: %v\n\n", len(bookings))
 
-		// iterate through bookings slice
-		// we get the index and value in each iteration
-		for _, booking := range bookings {
-			// splits string with white space as separator (we get an array with 2 elements)
-			var names = strings.Fields(booking)
-			// append first name(names[0]) to firstNames slice
-			firstNames = append(firstNames, names[0])
+			// define string slice
+			firstNames := []string{}
+
+			// iterate through bookings slice
+			// we get the index and value in each iteration
+			for _, booking := range bookings {
+				// splits string with white space as separator (we get an array with 2 elements)
+				var names = strings.Fields(booking)
+				// append first name(names[0]) to firstNames slice
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("First names of bookings: %v\n", firstNames)
+			fmt.Printf("Thank you %v for booking %v tickets, you will receive them in your email: %v\n", bookings[0], ticketAmount, email)
+			fmt.Printf("There are %v tickets left.\n\n", remainingTickets)
+
+			// if no more remaining tickets
+			if remainingTickets <= 0 {
+				fmt.Println("Our conference is booked. Come back next year")
+				// break out of loop (end program)
+				break
+			}	
+		} else {
+			// show error message
+			if !isValidName {
+				fmt.Println("Invalid name, please try again!")
+			} else if !isEmailValid {
+				fmt.Println("Invalid email, please try again!")
+			} else {
+				fmt.Println("Invalid ticket amount, please try again!")
+			}
+			// skipt to next loop iteration
+			continue
 		}
-
-		fmt.Printf("First names of bookings: %v\n", firstNames)
-		fmt.Printf("Thank you %v for booking %v tickets, you will receive them in your email: %v\n", bookings[0], ticketAmount, email)
-		fmt.Printf("There are %v tickets left.\n\n", remainingTickets)
 	}
 }
