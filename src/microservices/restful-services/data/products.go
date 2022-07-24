28caa72eabab82de86c2f031c5529209e822d1f7
Miguel Fernandes <migamado01@gmail.com>
2022-07-24 16:11:48 -0400

    Resful services in go/POST and PUT methods

--- src/microservices/restful-services/data/products.go
+++ src/microservices/restful-services/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -21,17 +22,71 @@ type Product struct {
 // type Products, list of Product
 type Products []*Product
 
-// ToJson function
+// ToJson function (uses Products, receives ioWriter, returns error)
 func (p *Products) ToJson(w io.Writer) error {
-	// instantiate new encoder with the receiver io writer
+	// instantiate new encoder with the received io writer
 	encoder := json.NewEncoder(w)
 	// encode products and possibly return error
 	return encoder.Encode(p)
 }
 
+// decode JSON and write to products (uses Product, receives ioReader, returns error)
+func (p *Product) FromJson(r io.Reader) error {
+	// instantiate new encoder with the received io reader
+	encoder := json.NewDecoder(r)
+	// write to our product struct the decoded json (may return an error)
+	return encoder.Decode(p)
+}
+
 // function that returns the product list
 func GetProducts() Products {
 	return productList
+}
+
+// receives a products and adds it 
+func AddProduct(p *Product) {
+	// assign ID of product (p) to next available int
+	p.ID = getNextId()
+	// append new item to productList
+	productList = append(productList, p)
+}
+
+// update product with given id, may return error
+func UpdateProduct(id int, p *Product) error {
+	// get product, position and error from findProduct
+	_, position, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	// set product id to id received
+	p.ID = id
+	// update product on the list
+	productList[position] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+// funct to find product in list, can return a product, position and an error
+func findProduct(id int) (*Product, int, error){
+	// iterate throught the product list
+	for i, p := range productList {
+		// if product is in the list, return it
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	// otherwise return error
+	return nil, -1, ErrProductNotFound
+}
+
+// function that return a next usable id
+func getNextId() int {
+	// get last product of the array
+	lastProduct := productList[len(productList) - 1]
+	// return ID + 1 of last product
+	return lastProduct.ID + 1
 }
 
 // create product list
--- src/microservices/restful-services/handlers/products.go
+++ src/microservices/restful-services/handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"main/src/microservices/restful-services/data"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -22,6 +24,38 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.getProducts(rw, r)
 		return
 	}
+	// if its a POST req
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+	// if its a PUT req
+	if r.Method == http.MethodPut {
+		// expect ID in url - extract it
+		regex := regexp.MustCompile(`/([0-9]+)`)
+		stringGroup := regex.FindAllStringSubmatch(r.URL.Path, -1)
+		// if stringGroup that matched that regex is not 1, http error
+		if len(stringGroup) != 1 {
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+		// if stringGroup[0] that matched that regex is not 2, http error
+		if len(stringGroup[0]) != 2 {
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		// get id from URL and convert to int
+		idString := stringGroup[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Unable to convert string to integer", http.StatusBadRequest)
+		}
+
+		// call updateProducts passing the id, rw and r
+		p.updateProduct(id, rw, r)
+		return
+	}
 
 	// catch all
 	rw.WriteHeader(http.StatusNotImplemented)
@@ -38,5 +72,46 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	// throw error if there was an error
 	if err != nil {
 		http.Error(rw, "Unable to convert to JSON", http.StatusInternalServerError)
+	}
+}
+
+// add product
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.log.Println("Handle POST req")
+	// allocate space in memory for a new "Product"
+	product := &data.Product{}
+	// call FromJson passing the req body
+	err := product.FromJson(r.Body)
+	// throw error if there was an error
+	if err != nil {
+		http.Error(rw, "Unable to convert from JSON", http.StatusBadRequest)
+	}
+
+	p.log.Printf("New Product: %#v", product)
+
+	// Add product
+	data.AddProduct(product)
+}
+
+//
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	// allocate space in memory for a new "Product"
+	product := &data.Product{}
+	// call FromJson passing the req body
+	err := product.FromJson(r.Body)
+	// throw error if there was an error
+	if err != nil {
+		http.Error(rw, "Unable to convert from JSON", http.StatusBadRequest)
+	}
+
+	// call updateProduct passing the id and product getting error back
+	err = data.UpdateProduct(id, product)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
 	}
 }
